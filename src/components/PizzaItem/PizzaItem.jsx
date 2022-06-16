da3ede34df9545ff316885a2be791d4554b127ce
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-16 14:31:27 -0500

    pizzaCart created

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 
 
+import { useSelector, useDispatch } from 'react-redux';
+
 function PizzaItem({ pizza }) {
+
+    const dispatch = useDispatch();
 
     const addPizzaToCart = () => {
         console.log(pizza);
-      }
+        // need to have this update a reducer with add in
+        dispatch ({
+            type: 'ADD_PIZZA_CART',
+            payload: pizza
+        })
+    }
+
+    const removePizzaToCart = () => {
+        console.log(pizza);
+        // need to have this update a reducer with removal
+        // dispatch a local array that pizza is saved to
+        dispatch ({
+            type: 'DELETE_PIZZA_CART',
+            payload: pizza
+        })
+
+    }
 
     return (
         <>
             <div>
                 {pizza.name}: {pizza.description} {pizza.price}
                 <button onClick={addPizzaToCart}>Add</button>
-                {/* <button onClick={removePizzaToCart}>Remove</button> */}
+                <button onClick={removePizzaToCart}>Remove</button>
             </div>
         </>
     )
