d15ebfe406857ffca1e33c484856dc8c75411e28
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-16 10:16:01 -0500

    store created

---
+++ TODO.md
@@ -0,0 +1,8 @@
+[x]DB
+    [x] create pizza_parlor db
+    [x] enter in sql sheet info
+
+[] APIs
+    [] /api/pizza
+    [] /api/order
+    [] /
--- src/index.js
+++ src/index.js
@@ -3,4 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+
+// get _order_list reducer for stephen create
+const orderList = (state = [], action) => {
+    switch (action.type) {
+        case 'GET_ORDER_LIST':
+            return action.payload
+    }
+}
+
+
+const pizzaList = (state = [], action) => {
+    switch (action.type) {
+        case 'GET_PIZZA_LIST':
+            return [...state, action.payload]
+    }
+}
+
+const storeInstance = createStore(
+    combineReducers({
+       pizzaList,
+       orderList
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+
